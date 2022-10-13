32d51e6bf4e02533c84fb49734eaeb4c89552374
Jonathan Almeida <90427766+JonathanAsf@users.noreply.github.com>
2022-10-13 04:56:11 -0300

    Condicional

--- adivinha.py
+++ adivinha.py
@@ -1,13 +1,24 @@
 print('*********************************')
 print('Bem vindo ao jogo de adivinhação')
 print('*********************************')
-
+#área de declaração de variáveis
 numeroSecreto=42
 suposicao=int(input('Digite o seu número número: '))
+acertou = numeroSecreto == suposicao
+maior = suposicao > numeroSecreto
+menor = suposicao < numeroSecreto
+
 print("Você digitou: ",suposicao)
 
+#Condição de acerto
 if numeroSecreto==suposicao:
   print('Você digitou o número correto!')
 else:
-  print('Você digitou o número errado')
+  if(maior):
+    print('Você errou! O seu chute foi maior que o número secreto.\n')
+  elif(menor):
+    print('Você errou! O seu chute foi menor que o número secreto.\n')
+
+print('*********************************')
 print('Fim do jogo')
+print('*********************************')
