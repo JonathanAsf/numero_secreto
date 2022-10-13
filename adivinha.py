ae543e5af8009d56ba8f8c3a323bdb75cbf7481b
Jonathan Almeida <90427766+JonathanAsf@users.noreply.github.com>
2022-10-13 07:08:20 -0300

    repetição
    Adição de um laço de repetição e organização do código

--- adivinha.py
+++ adivinha.py
@@ -1,24 +1,29 @@
 print('*********************************')
 print('Bem vindo ao jogo de adivinhação')
 print('*********************************')
-#área de declaração de variáveis
+#área de declaração de variáveis globais
 numeroSecreto=42
-suposicao=int(input('Digite o seu número número: '))
-acertou = numeroSecreto == suposicao
-maior = suposicao > numeroSecreto
-menor = suposicao < numeroSecreto
+total_de_tentativas = 3
+rodada=1
 
-print("Você digitou: ",suposicao)
+while (rodada <=total_de_tentativas > 0):
+  print('Tentivas restantes: ',total_de_tentativas)
+  suposicao=int(input('Digite o seu número número: '))
+  acertou = numeroSecreto == suposicao
+  maior = suposicao > numeroSecreto
+  menor = suposicao < numeroSecreto
+  print("Você digitou: ",suposicao)
 
-#Condição de acerto
-if numeroSecreto==suposicao:
-  print('Você digitou o número correto!')
-else:
-  if(maior):
-    print('Você errou! O seu chute foi maior que o número secreto.\n')
-  elif(menor):
-    print('Você errou! O seu chute foi menor que o número secreto.\n')
-
+  if numeroSecreto==suposicao:
+   print('*********************************')
+   print('Você digitou o número correto!')
+   print('*********************************')
+  else:
+    if(maior):
+      print('Você errou! O seu chute foi maior que o número secreto.')
+    elif(menor):
+      print('Você errou! O seu chute foi menor que o número secreto.')
+  total_de_tentativas = total_de_tentativas - 1
 print('*********************************')
 print('Fim do jogo')
 print('*********************************')
