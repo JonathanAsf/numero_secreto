60da12263a0684cf51fb5dfd479b547262ae8e48
Jonathan Almeida <90427766+JonathanAsf@users.noreply.github.com>
2022-10-18 01:25:30 -0300

    Melhora no código

--- adivinha.py
+++ adivinha.py
@@ -1,29 +1,41 @@
-print('*********************************')
-print('Bem vindo ao jogo de adivinhação')
-print('*********************************')
-#área de declaração de variáveis globais
-numeroSecreto=42
-total_de_tentativas = 3
-rodada=1
 
-while (rodada <=total_de_tentativas > 0):
-  print('Tentivas restantes: ',total_de_tentativas)
-  suposicao=int(input('Digite o seu número número: '))
+print(10 * "*")
+print('Bem vindo ao jogo de adivinhação')
+print(10 * "*")
+#área de declaração de variáveis globais
+numeroSecreto = 42
+total_de_tentativas = 3
+rodada= 1
+
+while (rodada <= total_de_tentativas):
+
+  print('Tentiva {} de {}'.format(rodada, total_de_tentativas))
+  suposicao=int(input('Digite o seu número:\n')) #Tentativa de acertar o numero secreto
+  print("Você digitou: ",suposicao)
+
   acertou = numeroSecreto == suposicao
   maior = suposicao > numeroSecreto
   menor = suposicao < numeroSecreto
-  print("Você digitou: ",suposicao)
 
+  #Caso de acerto
   if numeroSecreto==suposicao:
-   print('*********************************')
+
+   print(10 * "*")
+   print('Parabéns!')
    print('Você digitou o número correto!')
-   print('*********************************')
+   print(10 * "*")
+   
+   #Dica para o jogador
   else:
     if(maior):
       print('Você errou! O seu chute foi maior que o número secreto.')
     elif(menor):
+      rodada = rodada + 1
       print('Você errou! O seu chute foi menor que o número secreto.')
   total_de_tentativas = total_de_tentativas - 1
-print('*********************************')
-print('Fim do jogo')
-print('*********************************')
+
+  #Caso o número tentativas máximas foi alcançado.
+
+  if (rodada > total_de_tentativas):
+    print("Game Over, melhor sorte na próxima vez")
+    break
