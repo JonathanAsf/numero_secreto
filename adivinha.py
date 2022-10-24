3444a27f0c0ab8cd0fa77ac802a93134687a33cb
Jonathan Almeida <90427766+JonathanAsf@users.noreply.github.com>
2022-10-24 07:27:53 -0300

    Seleção de dificuldades
    Com esse update é possivel escolher a dificuldade, que baseia-se na quantidade de tentativas que o jogador terá.
    Agora também será exibido a pontuação que o jogador fez ao completar o jogo, começando em 1000 e subtraindo a cada tentativa falha.

--- adivinha.py
+++ adivinha.py
@@ -1,14 +1,30 @@
 import random
 
-
 print(10 * "*")
 print('Bem vindo ao jogo de adivinhação')
 print(10 * "*")
 
-#área de declaração de variáveis globais
-numero_secreto = round(random.randrange(1, 101))
-total_de_tentativas = 3
+#Área de declaração de variáveis globais
+numero_secreto = round(random.randrange(1, 101)) #Numeros aleatórios com range de 1 à 100
+total_de_tentativas = 0
+pontos = 1000
 
+#Seleção de dificuldade
+print('Selecione um nível de dificuldade: ')
+print('1 - Fácil')
+print('2 - Médio')
+print('3 - Difícil')
+
+dificuldade = int(input(''))
+
+if dificuldade == 1:
+  total_de_tentativas = 20
+elif dificuldade == 2:
+  total_de_tentativas = 10
+else:
+  total_de_tentativas = 5
+
+#Laço de repetição das chances
 for rodada in range (1, total_de_tentativas + 1 ):
   
   print('Tentiva {} de {}'.format(rodada, total_de_tentativas))
@@ -29,12 +45,15 @@ for rodada in range (1, total_de_tentativas + 1 ):
    print('Parabéns!')
    print('Você digitou o número correto!')
    print(10 * "*")
+   print("A sua pontuação foi {}!\n".format(pontos))
    break
    #Dica para caso de erro
   else:
+
     if(maior):
       print('Você errou! O seu chute foi maior que o número secreto.')
     elif(menor):
       print('Você errou! O seu chute foi menor que o número secreto.')
-  
+    pontos_perdidos = abs(numero_secreto - suposicao) #Valor absoluto  para não haver pontuação negativa
+    pontos = pontos - pontos_perdidos
 print('Fim de jogo')
